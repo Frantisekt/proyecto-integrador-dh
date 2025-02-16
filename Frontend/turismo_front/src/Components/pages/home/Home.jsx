bfb7488acf44317c4abb6777295906b3bbcb5f40
IsaMontoya17 <isabelamontoya2021@gmail.com>
2025-02-15 22:51:36 -0500

    HU-06
    Feat(web) Add new view

--- Frontend/turismo_front/src/Components/pages/home/Home.jsx
+++ Frontend/turismo_front/src/Components/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./Home.module.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className={styles.container}>Home</div>
+    )
 }
 
 export default Home
