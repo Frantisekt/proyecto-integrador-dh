16beca5878a8d8fdd1f019b38e5f704ee560259e
Sara Castañeda <saracastanedaecheverri@gmail.com>
2025-02-16 16:59:05 -0500

    HU-04
    categories container not finished yet

--- Frontend/turismo_front/src/Components/pages/home/Home.jsx
+++ Frontend/turismo_front/src/Components/pages/home/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Hero from "../../Hero/Hero"; 
+import Categories from "../../Categories/Categories";
 import styles from "./Home.module.css"; 
 
 const Home = () => {
   return (
     <div className={styles.container}>
-  
       <Hero />
-
-     
+      <Categories />
     </div>
   );
 };
