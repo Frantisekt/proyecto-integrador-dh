955c9b2ff7865b03ee432092bedd70d57838cf09
branca666 <ezequielauge@hotmail.com>
2025-03-16 18:45:25 -0300

    Subiendo cambios para Railway

--- Frontend/turismo_front/src/Components/Footer/Footer.jsx
+++ Frontend/turismo_front/src/Components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
-import style from './Footer.module.css';
+import style from './Footer.module.css'
+import logo from "../../assets/Logo_Final.png";
 
 const Footer = () => {
   return (
     <div className={style.footerWrapper}>
       <div className={style.logo}>
-        <img src="src/assets/Logo_Final.png" alt="Globe On Click" />
+        <img src={logo || "/placeholder.svg"} alt="Globe on Click" className={style.logo} />
       </div>
       <footer className={style.footer}>
         <p className={style.copyright}>Copyright © 2025 Globe On Click</p>
