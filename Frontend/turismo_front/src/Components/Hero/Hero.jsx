f70ca41769312530d8e095b839a1ff7558a77779
Sara Castañeda <saracastanedaecheverri@gmail.com>
2025-03-19 15:45:49 -0500

    date filter

--- Frontend/turismo_front/src/Components/Hero/Hero.jsx
+++ Frontend/turismo_front/src/Components/Hero/Hero.jsx
@@ -1,18 +1,31 @@
+// Hero.js
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
+  const [destination, setDestination] = useState("");
+  const [travelers, setTravelers] = useState(1);
+  const [startDate, setStartDate] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    if (!startDate) {
+      alert("Por favor, selecciona una fecha de inicio.");
+      return;
+    }
+    navigate(`/search-results?destination=${destination}&travelers=${travelers}&startDate=${startDate}`);
+  };
+
   return (
     <section className={styles.hero}>
       <div className={styles.overlay}></div>
-
       <div className={styles.content}>
         <div className={styles.heroTextContainer}>
           <h2 className={styles.heroText}>
-          Si estás en busca de aventuras emocionantes, experiencias inolvidables 
-          y lugares impresionantes para explorar, ¡has llegado al lugar indicado!
+            Si estás en busca de aventuras emocionantes, experiencias inolvidables y lugares impresionantes para explorar, ¡has llegado al lugar indicado!
           </h2>
         </div>
-
         <div className={styles.searchWrapper}>
           <div className={styles.searchContainer}>
             <div className={styles.searchInner}>
@@ -20,27 +33,17 @@ const Hero = () => {
               <div className={styles.searchBar}>
                 <div className={styles.inputGroup}>
                   <label>Buscar destino*</label>
-                  <input 
-                    type="text" 
-                    placeholder="Ingrese destino" 
-                  />
+                  <input type="text" placeholder="Ingrese destino" value={destination} onChange={(e) => setDestination(e.target.value)} />
                 </div>
                 <div className={styles.inputGroup}>
                   <label>Nro. de viajeros*</label>
-                  <input 
-                    type="number" 
-                    placeholder="Nro. de viajeros" 
-                    min="1"
-                  />
+                  <input type="number" placeholder="Nro. de viajeros" min="1" value={travelers} onChange={(e) => setTravelers(e.target.value)} />
                 </div>
                 <div className={styles.inputGroup}>
                   <label>Fecha prevista*</label>
-                  <input 
-                    type="date" 
-                    placeholder="MM / YYYY" 
-                  />
+                  <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
                 </div>
-                <button type="button">BUSCAR</button>
+                <button type="button" onClick={handleSearch}>BUSCAR</button>
               </div>
             </div>
           </div>
